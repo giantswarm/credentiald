4df66758e3155447ae9273264870505c0f1db98d
Theo Brigitte <theo.brigitte@gmail.com>
2019-01-31 17:10:51 +0100

    split methods into corresponding files

---
+++ client/search.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/giantswarm/credentiald/service/searcher"
+	"github.com/giantswarm/microerror"
+)
+
+func (c Client) Search(ctx context.Context, request searcher.Request) (*searcher.Response, error) {
+	var response searcher.Response
+	{
+		req := c.restClient.R()
+
+		u, err := c.url.Parse(fmt.Sprintf(CredentialDetailsEndpoint, request.Organization, request.ID))
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		resp, err := c.do(ctx, req.Get, u.String())
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		if resp.StatusCode() != http.StatusOK {
+			return nil, microerror.Maskf(invalidRequestError, string(resp.Body()))
+		}
+
+		err = json.Unmarshal(resp.Body(), &response)
+		if err != nil {
+			return nil, microerror.Maskf(invalidResponseError, string(resp.Body()))
+		}
+	}
+	return &response, nil
+
+}
