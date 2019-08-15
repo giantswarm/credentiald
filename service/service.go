d4158401c96e89dd7c89d2f160fd0781714bfe4c
Tuomas Mäkinen <tuomas@giantswarm.io>
2019-08-15 13:17:49 +0300

    Update microkit service configuration
    Latest state of the art has separate `pkg/project` for configuring microkit
    service so that's added here.

--- service/service.go
+++ service/service.go
@@ -28,6 +28,7 @@ type Config struct {
 	GitCommit   string
 	ProjectName string
 	Source      string
+	Version     string
 }
 
 type Service struct {
@@ -148,6 +149,7 @@ func New(config Config) (*Service, error) {
 			GitCommit:   config.GitCommit,
 			Name:        config.ProjectName,
 			Source:      config.Source,
+			Version:     config.Version,
 		}
 
 		versionService, err = version.New(c)
