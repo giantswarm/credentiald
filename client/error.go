337b9faff0759b9a2d7d380d474885840d68f16b
wtty-fool <wtty.fool@gmail.com>
2020-04-10 16:20:44 +0200

    migrate away from microerror.New

--- client/error.go
+++ client/error.go
@@ -4,21 +4,27 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var invalidConfigError = microerror.New("invalid config")
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
-var invalidRequestError = microerror.New("invalid request")
+var invalidRequestError = &microerror.Error{
+	Kind: "invalidRequestError",
+}
 
 // IsInvalidRequest asserts invalidRequestError.
 func IsInvalidRequest(err error) bool {
 	return microerror.Cause(err) == invalidRequestError
 }
 
-var invalidResponseError = microerror.New("invalid response")
+var invalidResponseError = &microerror.Error{
+	Kind: "invalidResponseError",
+}
 
 // IsInvalidResponse asserts invalidResponseError.
 func IsInvalidResponse(err error) bool {
