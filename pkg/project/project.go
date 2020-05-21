72e688c71bd666e542a272d8bb815b57d6a89adf
Jakub Soboń <wtty.fool@gmail.com>
2020-05-21 17:07:31 +0200

    bump version to 0.3.1-dev (#54)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "credentiald"
 	source      = "https://github.com/giantswarm/credentiald"
-	version     = "0.3.0"
+	version     = "0.3.1-dev"
 )
 
 func Description() string {
