5483dfbea41c4049b0c1bd18a699281cca2dcfaf
GitHub Action <action@github.com>
2021-06-09 09:55:06 +0000

    Bump version to 2.1.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "credentiald"
 	source      = "https://github.com/giantswarm/credentiald"
-	version     = "2.1.0"
+	version     = "2.1.1-dev"
 )
 
 func Description() string {
