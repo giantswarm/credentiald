ec1411793d853a2744bfbd395c515459edeabc18
taylorbot <dev@giantswarm.io>
2023-12-22 10:56:36 +0000

    Bump version to 2.3.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "credentiald"
 	source      = "https://github.com/giantswarm/credentiald"
-	version     = "2.3.1"
+	version     = "2.3.2-dev"
 )
 
 func Description() string {
